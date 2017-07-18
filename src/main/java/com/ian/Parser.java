f49a96169cdb17878a6df1ef715aac9d8f2a365e
Ian Carroll <ian.d.carroll@gmail.com>
2017-07-18 13:20:28 -0700

    test content past request-line is ignored

--- src/main/java/com/ian/Parser.java
+++ src/main/java/com/ian/Parser.java
@@ -2,7 +2,7 @@ package com.ian;
 
 public class Parser {
     public static String parse(String request) {
-        String[] splitReq = request.split(" ");
+        String[] splitReq = request.split("\\s");
         return "HTTP/1.1 " + Chef.plate(splitReq[1]);
     }
 }
--- src/test/java/com/ian/ParserTest.java
+++ src/test/java/com/ian/ParserTest.java
@@ -12,4 +12,10 @@ public class ParserTest {
     @Test
     public void testParserReturns404ResponseWhenAnythingOtherThanRootIsRequested()
     { assertEquals("HTTP/1.1 404 Not Found", Parser.parse("GET /A-day-not-night-to-see-till-I-see-thee HTTP/1.1")); }
+
+    @Test
+    public void testParserIgnoresAdditionalContentPastTheRequestLine()
+    { assertEquals("HTTP/1.1 200 OK", Parser.parse("GET / HTTP/1.1\n" +
+                                                                    "Host: [rsid].112.2o7.net\n" +
+                                                                    "X-Forwarded-For: 192.168.10.1")); }
 }

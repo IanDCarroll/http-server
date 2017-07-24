a4fe5b01a573b84728bd097e52e6bb4f46edec40
Ian Carroll <ian.d.carroll@gmail.com>
2017-07-24 10:47:50 -0700

    delete commented-out println statements

--- src/test/java/com/ian/ClientSocketHelper.java
+++ src/test/java/com/ian/ClientSocketHelper.java
@@ -6,14 +6,11 @@ import java.net.*;
 public class ClientSocketHelper {
 
     public static String request(String toServer) throws IOException {
-        //System.out.println("request called");
         Socket clientSocket = new Socket("127.0.0.1", 5000);
-        //System.out.println("ClientSocket initiated");
         PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         outToServer.println(toServer);
-        //System.out.println("Client called");
         return inFromServer.readLine();
     }
 }

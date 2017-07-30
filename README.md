285ef9646069e31fd10ae312b207dcce8f6831c3
Ian Carroll <ian.d.carroll@gmail.com>
2017-07-30 00:48:36 -0700

    Run integration tests in separate thread

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # http-server
 Java HTTP server implementing https://github.com/8thlight/cob_spec
+
+#### how to build the server:
+clone repo
+`$ mvn clean install`
+
+#### how to run the tests:
+replace `/PATH/TO/` with your absolute paths to the repo and maven
+`$ java -classpath /PATH/TO/http-server/target/test-classes:/PATH/TO/http-server/target/classes:/PATH/TO/.m2/repository/junit/junit/4.12/junit-4.12.jar:/PATH/TO/.m2/repository/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar com.ian.RunTests`
+
+#### how to start the server:
+`$ java -jar target/http-server-1.0-SNAPSHOT.jar [optional port(default 5000)]`
+connect locally with a browser or client socket: `localhost:5000`
--- src/test/java/com/ian/RunTests.java
+++ src/test/java/com/ian/RunTests.java
@@ -1,9 +1,80 @@
 package com.ian;
 
 import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
+
 
 public class RunTests {
+    private static Result unit;
+    private static Result intg;
+
+    private static int runs;
+    private static int ignores;
+    private static int fails;
+    private static List<Failure> failReport;
+    private static boolean success;
+    private static long time;
+
     public static void main(String[] args) {
-        JUnitCore.runClasses(IntegrationTest.class);
+        runIntegrationTests();
+        runUnitTests();
+
+        aggregateResults();
+
+        printResults();
+    }
+
+    private static void runUnitTests() {
+        unit = JUnitCore.runClasses(ParserTest.class, ChefTest.class);
+    }
+
+    private static void runIntegrationTests() {
+        Runnable intgRunnable = () -> intg = JUnitCore.runClasses(IntegrationTest.class);
+        Thread intgThread = new Thread(intgRunnable);
+        intgThread.start();
+        try { intgThread.join();
+        } catch (InterruptedException e) { e.getMessage(); }
+    }
+
+    private static void aggregateResults() {
+        runs = unit.getRunCount() + intg.getRunCount();
+        ignores = unit.getIgnoreCount() + intg.getIgnoreCount();
+        fails = unit.getFailureCount() + intg.getFailureCount();
+
+        long unitTime = unit.getRunTime();
+        long intgTime = intg.getRunTime();
+        time = unitTime > intgTime ? unitTime : intgTime;
+
+        success = unit.wasSuccessful() && intg.wasSuccessful();
+
+        List<Failure> unitFailures = unit.getFailures();
+        List<Failure> intgFailures = intg.getFailures();
+        unitFailures.addAll(intgFailures);
+        failReport = unitFailures;
+    }
+
+    private static void printResults() {
+        System.out.format("testing complete:\n");
+        System.out.format("%d tests; %d ignored; with %d failures in %d miliseconds\n",
+                          runs, ignores, fails, time);
+        if (success) {
+            System.out.println("All tests are passing.");
+        } else {
+            printFailures();
+        }
+    }
+
+    private static void printFailures() {
+        System.out.format("There were failures...\n");
+        for (Failure failure : failReport) {
+            System.out.format("\nThis was a thing that failed:\n");
+            System.out.println(failure);
+        }
     }
 }

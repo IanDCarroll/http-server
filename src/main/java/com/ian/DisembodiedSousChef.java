f9802685496dbefeeccabb283e6c66f4b1048055
Ian Carroll <ian.d.carroll@gmail.com>
2017-09-12 14:03:22 -0700

    refactor names in DisembodiedSousChef to reflect reality

--- src/main/java/com/ian/DisembodiedSousChef.java
+++ src/main/java/com/ian/DisembodiedSousChef.java
@@ -2,14 +2,14 @@ package com.ian;
 
 public class DisembodiedSousChef {
     public static byte[] craftResponseHead(String directory, String request, byte[] body) {
-        byte[] startLine = craftStartLine(directory, request);
-        byte[] headers = HeadersCook.craftStandardHeaders(directory, request, body);
+        byte[] responseLines = craftResponseLines(directory, request);
+        byte[] standardHeaders = HeadersCook.craftStandardHeaders(directory, request, body);
         byte[] crlf = "\r\n\r\n".getBytes();
-        byte[][] responseHead = { startLine, headers, crlf };
+        byte[][] responseHead = { responseLines, standardHeaders, crlf };
         return ByteArrayCook.concatenateByteArrays(responseHead);
     }
 
-    public static byte[] craftStartLine(String directory, String request) {
+    public static byte[] craftResponseLines(String directory, String request) {
         if (RedirectLister.checkRedirect(request)) {
             return RedirectCook.craft302Response(request);
         } else if (HTCPCPLister.checkTea(request)) {
